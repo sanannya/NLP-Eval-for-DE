352d47d28fc8d2054fefc0bfc88a346ad147584c
sanannya <anannya.sathaye@gmail.com>
2025-05-20 20:11:19 -0500

    got all final 400 pain points data

--- Example/example.py
+++ Example/example.py
@@ -3,8 +3,4 @@ from NLP_Eval_for_DE import helpers, process_data, results
 # results.get_predictions("Jina Embeddings V2", "C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\Pain points data-ground truth.txt", "C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\pain_point_codes_desc", "pred_test3")
 # results.get_scores("Jina Embeddings V2", "C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\Pain points data-ground truth.txt", "C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\pain_point_codes_desc", "score_test2")
 
-results.get_evaluation("C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\pain points full.csv", "C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\short titles.csv")
-# testable_data = process_data.get_testable_data("C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\pain points full.csv")
-# codes = process_data.get_codes("C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\test codes data.csv")
-# predictions = helpers.get_BERT_scores(testable_data[0], codes[0])
-# print(helpers.evaluate(testable_data[1], predictions[0], codes[1])[1])
+results.get_evaluation("C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\pain points full.csv", "C:\\Users\\engrla\\NLP-Eval-for-DE\\Example\\short titles+descriptions.csv")
