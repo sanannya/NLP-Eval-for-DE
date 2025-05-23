5c51ad8267ecf8c29bf21124380279a19cfe0842
sanannya <anannya.sathaye@gmail.com>
2025-05-23 01:32:56 -0500

    mini trial w/ Jina on new hackathon data, added written to numerical code conversion function

--- NLP_Eval_for_DE/process_data.py
+++ NLP_Eval_for_DE/process_data.py
@@ -51,3 +51,34 @@ def make_dataset(testable_data):
     }
     dataset = Dataset.from_dict(data)
     return dataset
+
+def written_code_to_numbers(datafile, codefile):
+    #if your ground truths are in written instead of numbered format, use this first to get the correct formatting
+    #will assign code numbers sequentially based on the codebook file
+    #will create a new input data file, but the codebook file stays the same
+    codes = get_codes(codefile)[0]
+    new_data = []
+
+    with open(datafile, 'r') as infile, open("new datafile.csv", 'w', newline='') as outfile:
+        reader = csv.DictReader(infile)
+        new_val = "0"
+        for row in reader:
+            categories = list(row.keys())
+            new_row = row
+            print(row)
+            for i in range(len(codes)):
+                if (codes[i] == row[categories[1]]):
+                    # print("match!")
+                    # print(row[categories[1]])
+                    # print(codes[i])
+                    new_val = str(i)
+                    #print(new_row[categories[1]])
+            if (row[categories[1]] not in codes):
+                new_val = "0"
+            new_row[categories[1]] = new_val
+            #print(new_row)
+            new_data.append(new_row)
+
+        writer = csv.DictWriter(outfile, fieldnames=categories)
+        writer.writeheader()
+        writer.writerows(new_data)
